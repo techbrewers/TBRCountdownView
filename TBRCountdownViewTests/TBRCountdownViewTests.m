73e0aafb7240ab669a9bab934d059703d09a1ab0
Luciano Marisi <Luciano.Marisi@monitise.com>
2014-07-08 11:49:47 +0100

    Initial Commit

---
+++ TBRCountdownViewTests/TBRCountdownViewTests.m
@@ -0,0 +1,34 @@
+//
+//  TBRCountdownViewTests.m
+//  TBRCountdownViewTests
+//
+//  Created by Luciano Marisi on 08/07/2014.
+//  Copyright (c) 2014 TechBrewers LTD. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TBRCountdownViewTests : XCTestCase
+
+@end
+
+@implementation TBRCountdownViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
